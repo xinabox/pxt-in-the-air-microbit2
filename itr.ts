64689f014b0f0e53eb56184068d173693955b6e5
KalbeAbbas <kalbeabbas@142@gmail.com>
2020-11-25 10:29:20 +0500

    SG35 removed

--- itr.ts
+++ itr.ts
@@ -247,8 +247,6 @@ namespace ITR
     // SN01 function call end
 
     // SG35 function call start
-
-    begin()
     
     // SG35 function call end
 
@@ -1434,69 +1432,5 @@ namespace ITR
             }
         }
     }
-	
-
-    //%shim=sg35::begin
-    function begin(): void {
-        return
-    }
-
-    //%shim=sg35::read
-    export function read(): boolean {
-        return true
-    }
-
-
-    //%shim=sg35::pm1
-    export function pm1(): number {
-        return 1
-    }
-
-
-    //%shim=sg35::pm25
-    export function pm25(): number {
-        return 1
-    }
-
-
-    //%shim=sg35::pm10
-    export function pm10(): number {
-        return 1
-    }
-
-    //%shim=sg35::onDataReceived
-    function onDataReceived(body: Action): void {
-        return;
-    }
-
-    function init() {
-
-        startParallel(function(){
-                while(true)
-                {
-                    let rcv = read()
-                    if(rcv)
-                    {
-                        onReceivedDataHandler(pm1(), pm25(), pm10())
-                    }
-                    basic.pause(100)
-                }
-        })
-    }
-
-    //% block="SG35 on received "
-    //% group="SG35"
-    //% draggableParameters=reporter
-    export function onReceivedData(cb: (receivedPM1: number,receivedPM25: number,receivedPM10: number) => void): void {
-        init()
-        onReceivedDataHandler = cb
-    }
-
-    //% shim=parall::startParallel
-    export function startParallel(u: () => void) {
-        return 1;
-    }
-
-    begin()
 
 }
